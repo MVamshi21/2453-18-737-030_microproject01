bd8d85042cfb908ebef519a05d5566e430dfb058
vamshi2100 <63459122+vamshi2100@users.noreply.github.com>
2020-10-08 12:17:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ let middleware = require('./middleware');
 const response = require('express');
 
 const url = 'mongodb://127.0.0.1:27017';
-const dbname = "MP1";
+const dbname = "HospitalManagement";
 let db
 
 MongoClient.connect(url, (err, client) => {
